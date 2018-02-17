8fc26c7a5c7f320de1b69a8b4d4ce35939f1782d
marcus koh <kohchihao@gmail.com>
2018-02-18 02:10:58 +0800

    Edited package.json and added static folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: ['gatsby-plugin-react-helmet'],
-  
-  pathPrefix: `/kohchihao-website-v2`,
+  pathPrefix: '/kohchihao-website-v2',
   
 };
